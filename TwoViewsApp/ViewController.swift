a2f94b484bc4ba7a5e53cec6b64f5586c62709a7
Dmitriy Starozhilov <dimstar-90@mail.ru>
2022-03-08 20:43:31 +0500

    Initial Commit

---
+++ TwoViewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViewsApp
+//
+//  Created by Dmitriy Starozhilov on 08.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
